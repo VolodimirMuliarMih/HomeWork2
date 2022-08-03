2676da2d00bdb10a54eac9c3e09218049bcc0442
VolodimirMuliarMih <dobroslaf777@gmail.com>
2022-08-03 10:41:17 +0300

    #Homework

---
+++ main.py
@@ -0,0 +1,8 @@
+#HomeWork2
+print("Введите строку для дальнейшей обработки\n")
+name=input("")
+name1=str(name[1::2])
+name2=str(name1[::-1]).upper()
+print(name1)
+print(name2)
+input("Нажмите Enter для выхода из программы")
